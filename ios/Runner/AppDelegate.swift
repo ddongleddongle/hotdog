56a0a124348c6db2f39fba7b21b2a7ea84b58916
ddongleddongle <gonzar12@naver.com>
2024-11-10 15:14:19 +0900

    polylines

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -7,6 +7,7 @@ import UIKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    GMSServices.provideAPIKEY("AIzaSyDenPclJquav9-fQFtHsjnSIvMN1ORoOq0")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
