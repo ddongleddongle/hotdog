7f45a3323f2ec24f3998710f77b6767ca3b2b30a
Huiong1 <gmldnd3964@donga.ac.kr>
2024-11-11 03:42:56 +0900

    product-detail

--- lib/Shop/ProductClass.dart
+++ lib/Shop/ProductClass.dart
@@ -1,10 +1,11 @@
 class ProductClass {
   final String name;
-  final double price;
+  final int price;
   final double rating;
   final String imageURL;
   final double reviews;
   final double matchScore;
+  final String description;
 
   ProductClass({
     required this.name,
@@ -13,16 +14,18 @@ class ProductClass {
     required this.imageURL,
     required this.reviews, // 생성자에서 필수로 초기화
     required this.matchScore,
+    required this.description,
   });
 
   factory ProductClass.fromJson(Map<String, dynamic> json) {
     return ProductClass(
       name: json['name'],
-      price: json['price'].toDouble(), // double로 변환
+      price: json['price'],
       rating: json['rating'].toDouble(), // double로 변환
       imageURL: json['imageURL'],
       reviews: json['reviews'].toDouble(),
       matchScore: json['matchScore'].toDouble(),
+      description: json['description'],
     );
   }
 }
--- lib/Shop/product_grid.dart
+++ lib/Shop/product_grid.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'product_item.dart';
-import 'product-detail/product.dart'; // Product.dart를 import합니다.
+import 'product-detail/ProductDetail.dart'; // ProductDetail 페이지를 import합니다.
 import 'ProductClass.dart'; // ProductClass 모델을 import합니다.
 
 class ProductGrid extends StatelessWidget {
@@ -18,11 +18,7 @@ class ProductGrid extends StatelessWidget {
           children: products.map((product) {
             return _buildProductColumn(
               context,
-              product.imageURL, // 상품 이미지 URL
-              product.rating, // 상품 평점
-              product.reviews,
-              product.name,
-              product.matchScore,
+              product, // ProductClass 인스턴스를 전달
             );
           }).toList(),
         ),
@@ -30,26 +26,28 @@ class ProductGrid extends StatelessWidget {
     );
   }
 
-  Widget _buildProductColumn(BuildContext context, String imageURL,
-      double rating, double reviews, String name, double matchScore) {
+  Widget _buildProductColumn(BuildContext context, ProductClass product) {
     return GestureDetector(
       // 클릭 감지
       onTap: () {
         Navigator.push(
           context,
           MaterialPageRoute(
-            builder: (context) => product(), // Product.dart의 ProductPage로 이동
+            builder: (context) =>
+                ProductDetail(product: product), // ProductDetail로 이동
           ),
         );
       },
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          ProductItem(imageURL: imageURL),
+          ProductItem(imageURL: product.imageURL), // 상품 이미지 URL
           const SizedBox(height: 8),
-          ProductRating(rating: rating, reviews: reviews),
+          ProductRating(
+              rating: product.rating, reviews: product.reviews), // 상품 평점
           const SizedBox(height: 4),
-          ProductInfo(name: name, matchScore: matchScore),
+          ProductInfo(
+              name: product.name, matchScore: product.matchScore), // 상품 정보
         ],
       ),
     );
